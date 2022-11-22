c3f6e20dc849219e9cd20a45485e9a35d05f3037
balajibadly <balajibalamurugan236@gmail.com>
2022-11-22 18:24:15 +0530

    refactor(explore): :fire: control value accessor
    none
    
    none

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommunicationPreferenceComponent } from "./custom-comp";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [
+        RouterModule,
+        CommunicationPreferenceComponent,
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class AppComponent {
-  title = 'v15';
+  communication = new FormControl([
+    {
+      label: 'Marketing',
+      modes: [
+        {
+          name: 'Email',
+          enabled: true,
+        },
+        {
+          name: 'SMS',
+          enabled: false,
+        },
+      ],
+    },
+    {
+      label: 'Product Updates',
+      modes: [
+        {
+          name: 'Email',
+          enabled: true,
+        },
+        {
+          name: 'SMS',
+          enabled: true,
+        },
+      ],
+    },
+  ]
+  );
+
+  toogle(): void {
+    if(this.communication.disabled) {
+      this.communication.enable();
+    }else {
+      this.communication.disable();
+    }
+  }
 }
---
+++ src/app/test/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'test',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export default class TestComponent {
+
+}
